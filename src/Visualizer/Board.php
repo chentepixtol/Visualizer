3978a63422b84a1c860b067a1ac852b6c82e76ee
Vicente Mendoza <vicentemmor@yahoo.com.mx>
2015-02-12 07:55:18 -0600

    command show

--- src/Visualizer/Board.php
+++ src/Visualizer/Board.php
@@ -4,23 +4,47 @@ namespace Visualizer;
 
 class Board
 {
+    const WHITE = 'O';
+
     protected $width;
 
     protected $height;
 
-	public function __construct($width, $height)
-	{
-		$this->width = $width;
-		$this->height = $height;
-	}
+    protected $coordinates = array();
 
-	public function getWidth()
-	{
-	    return $this->width;
-	}
+    public function __construct($width, $height)
+    {
+        $this->width = $width;
+        $this->height = $height;
+    }
 
-	public function getHeight()
-	{
-	    return $this->height;
-	}
+    public function getWidth()
+    {
+        return $this->width;
+    }
+
+    public function getHeight()
+    {
+        return $this->height;
+    }
+
+    public function getCoordinate($x, $y, $default = null)
+    {
+        return isset($this->coordinates[$x][$y]) ? $this->coordinates[$x][$y] : $default;
+    }
+
+    /**
+     * @return string
+     */
+    public function show()
+    {
+        $str = "";
+        for($y=1; $y<=$this->getHeight(); $y++) {
+            for($x=1; $x<=$this->getWidth(); $x++) {
+                $str .= $this->getCoordinate($x, $y, static::WHITE);
+            }
+            $str .= "\n";
+        }
+        return $str;
+    }
 }
--- test/BoardTest.php
+++ test/BoardTest.php
@@ -7,14 +7,23 @@ use Visualizer;
 
 class BoardTest extends \PHPUnit_Framework_TestCase
 {
-	
-	/**
-	 * @test
-	 */
-	public function initialization()
-	{
-	   $board = new Visualizer\Board(10, 10);
-	   $this->assertEquals(10, $board->getWidth());
-	   $this->assertEquals(10, $board->getHeight());
-	}
+    
+    /**
+     * @test
+     */
+    public function initialization()
+    {
+       $board = new Visualizer\Board(10, 10);
+       $this->assertEquals(10, $board->getWidth());
+       $this->assertEquals(10, $board->getHeight());
+    }
+
+    /**
+     * @test
+     */
+    public function showBoard()
+    {
+        $board = new Visualizer\Board(3, 4);
+        $this->assertEquals("OOO\nOOO\nOOO\nOOO\n", $board->show());
+    }
 }
